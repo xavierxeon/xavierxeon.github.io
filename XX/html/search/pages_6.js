e9f8181b525f27d6506e0c69d7597828f989554f
Ralf Waspe <rwaspe@icloud.com>
2026-02-14 11:47:16 +0100

    Update documentation

--- XX/html/search/pages_6.js
+++ XX/html/search/pages_6.js
@@ -1,13 +1,4 @@
 var searchData=
 [
-  ['xx_20cmake_20package_0',['XX CMake Package',['../index.html',1,'XX CMake Package'],['../index.html#autotoc_md0',1,'XX CMake Package']]],
-  ['xxapplication_1',['XXApplication',['../group___x_x_application.html#autotoc_md3',1,'']]],
-  ['xxaudio_2',['XXAudio',['../group___x_x_audio.html#autotoc_md4',1,'']]],
-  ['xxgraph_3',['XXGraph',['../group___x_x_graph.html#autotoc_md5',1,'']]],
-  ['xxgui_4',['XXGui',['../group___x_x_gui.html#autotoc_md6',1,'']]],
-  ['xxlog_5',['XXLog',['../group___x_x_log.html#autotoc_md7',1,'']]],
-  ['xxmath_6',['XXMath',['../group___x_x_math.html#autotoc_md8',1,'']]],
-  ['xxmusic_7',['XXMusic',['../group___x_x_music.html#autotoc_md9',1,'']]],
-  ['xxnetwork_8',['XXNetwork',['../group___x_x_network.html#autotoc_md10',1,'']]],
-  ['xxwidgets_9',['XXWidgets',['../group___x_x_widgets.html#autotoc_md11',1,'']]]
+  ['xx_20cmake_20package_0',['XX CMake Package',['../index.html',1,'XX CMake Package'],['../index.html#autotoc_md0',1,'XX CMake Package']]]
 ];
