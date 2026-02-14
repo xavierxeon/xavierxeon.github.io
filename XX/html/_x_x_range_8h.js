38253bbb2b09a6dca5320797a55cb6d8e9b96fa1
Ralf Waspe <rwaspe@icloud.com>
2026-02-14 11:20:39 +0100

    Update documentation

--- XX/html/_x_x_range_8h.js
+++ XX/html/_x_x_range_8h.js
@@ -2,8 +2,8 @@ var _x_x_range_8h =
 [
     [ "XX::Range", "struct_x_x_1_1_range.html", "struct_x_x_1_1_range" ],
     [ "XX::Range::Spread::Iterator", "class_x_x_1_1_range_1_1_spread_1_1_iterator.html", "class_x_x_1_1_range_1_1_spread_1_1_iterator" ],
-    [ "XX::isFloatType", "namespace_x_x.html#aedd13f6317d564d69aaab085caf5a3f7", null ],
-    [ "XX::isIntegerType", "namespace_x_x.html#adeaf1c71a80b637e952a3c88bcd8f1b7", null ],
-    [ "XX::isSigned", "namespace_x_x.html#a785bbb775cb5374c07d3ad7958625aa6", null ],
-    [ "XX::isUnsigned", "namespace_x_x.html#a16f8071a2bec7511e9524ddffba58f95", null ]
+    [ "XX::isFloatType", "group___x_x_cpp.html#gaedd13f6317d564d69aaab085caf5a3f7", null ],
+    [ "XX::isIntegerType", "group___x_x_cpp.html#gadeaf1c71a80b637e952a3c88bcd8f1b7", null ],
+    [ "XX::isSigned", "group___x_x_cpp.html#ga785bbb775cb5374c07d3ad7958625aa6", null ],
+    [ "XX::isUnsigned", "group___x_x_cpp.html#ga16f8071a2bec7511e9524ddffba58f95", null ]
 ];
--- XX/html/group___x_x_cpp.js
+++ XX/html/group___x_x_cpp.js
@@ -69,5 +69,9 @@ var group___x_x_cpp =
       [ "Spread", "class_x_x_1_1_range_1_1_spread.html#a884ff1dd1430a263a7336673e61efb07", null ],
       [ "begin", "class_x_x_1_1_range_1_1_spread.html#aa727c056f8395b180f392fc736ad6ed0", null ],
       [ "end", "class_x_x_1_1_range_1_1_spread.html#a88b816d0894d740ed75e0aef3ace35cc", null ]
-    ] ]
+    ] ],
+    [ "XX::isFloatType", "group___x_x_cpp.html#gaedd13f6317d564d69aaab085caf5a3f7", null ],
+    [ "XX::isIntegerType", "group___x_x_cpp.html#gadeaf1c71a80b637e952a3c88bcd8f1b7", null ],
+    [ "XX::isSigned", "group___x_x_cpp.html#ga785bbb775cb5374c07d3ad7958625aa6", null ],
+    [ "XX::isUnsigned", "group___x_x_cpp.html#ga16f8071a2bec7511e9524ddffba58f95", null ]
 ];
