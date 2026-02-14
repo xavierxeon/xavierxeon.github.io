1829fb932e54682955a232eb9beb1b4a8f313b99
Ralf Waspe <rwaspe@icloud.com>
2026-02-14 03:24:04 +0100

    Update documentation

--- XX/html/group___x_x_widgets.js
+++ XX/html/group___x_x_widgets.js
@@ -1,5 +1,6 @@
 var group___x_x_widgets =
 [
+    [ "XX::Managed", "namespace_x_x_1_1_managed.html", null ],
     [ "XX::LocalTextBrowser", "class_x_x_1_1_local_text_browser.html", [
       [ "LocalTextBrowser", "class_x_x_1_1_local_text_browser.html#aa50e9d35b29766207b95f4f195304dfa", null ]
     ] ],
--- XX/html/topics.js
+++ XX/html/topics.js
@@ -1,6 +1,5 @@
 var topics =
 [
-    [ "@{", "group___x_x_widgets.html", "group___x_x_widgets" ],
     [ "XXAudio", "group___x_x_audio.html", null ],
     [ "XXCore", "group___x_x_core.html", null ],
     [ "XXCpp", "group___x_x_cpp.html", null ],
@@ -8,5 +7,6 @@ var topics =
     [ "XXGui", "group___x_x_gui.html", null ],
     [ "XXLog", "group___x_x_log.html", null ],
     [ "XXMath", "group___x_x_math.html", null ],
-    [ "XXNetwork", "group___x_x_network.html", null ]
+    [ "XXNetwork", "group___x_x_network.html", null ],
+    [ "XXWidgets", "group___x_x_widgets.html", "group___x_x_widgets" ]
 ];
