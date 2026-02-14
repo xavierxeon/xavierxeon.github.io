eb2be78775b644c7f9fb83565d74b58b2b9e57f9
Ralf Waspe <rwaspe@icloud.com>
2026-02-14 09:48:31 +0100

    Update documentation

--- XX/html/search/files_0.js
+++ XX/html/search/files_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['readme_2emd_0',['README.md',['../_r_e_a_d_m_e_8md.html',1,'(Global Namespace)'],['../_x_x_application_2_r_e_a_d_m_e_8md.html',1,'(Global Namespace)'],['../_x_x_audio_2_r_e_a_d_m_e_8md.html',1,'(Global Namespace)'],['../_x_x_cpp_2_r_e_a_d_m_e_8md.html',1,'(Global Namespace)'],['../_x_x_graph_2_r_e_a_d_m_e_8md.html',1,'(Global Namespace)'],['../_x_x_gui_2_r_e_a_d_m_e_8md.html',1,'(Global Namespace)'],['../_x_x_log_2_r_e_a_d_m_e_8md.html',1,'(Global Namespace)'],['../_x_x_math_2_r_e_a_d_m_e_8md.html',1,'(Global Namespace)'],['../_x_x_music_2_r_e_a_d_m_e_8md.html',1,'(Global Namespace)'],['../_x_x_network_2_r_e_a_d_m_e_8md.html',1,'(Global Namespace)']]]
+  ['readme_2emd_0',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]]
 ];
